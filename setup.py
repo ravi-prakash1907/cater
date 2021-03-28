9b96d0e1e65c267af18bef20e4766e5de93e2bab
Ravi Prakash <1907prakash.ravi@gmail.com>
2021-03-28 19:48:24 +0530

    link updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'cater', # pkg name
     version='0.1',
     description='A TCP based file sharing application',
-    url='git@github.com:ravi-prakash1907/cater.git', # post the http "clone" link
+    url='https://github.com/ravi-prakash1907/cater.git', # post the http "clone" link
     author='Ravi Prakash',
     author_email='ravi.cs20@iiitmk.ac.in',
     license='unlicense',
